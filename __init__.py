13f4a7dbc35fef323181696c81c9bf7290a194d4
Praveen Venkatesh <praveenv253@gmail.com>
2017-06-07 02:42:16 -0400

    Add an __init__.py file to make this a module.

---
+++ __init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+# The following overwrites the module name, so other functions in
+# polyproject.py would also have to be imported here in future.
+from .polyproject import polyproject
